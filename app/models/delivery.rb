03dcfbc1155d7ff676dc5b5ebaf1911bce50f6cc
Prateep Kul <1.0@kul.com>
2014-01-24 22:35:13 +0700

    Add fields following Picture of the report delivery.jpg

--- app/models/delivery.rb
+++ app/models/delivery.rb
@@ -10,5 +10,15 @@ class Delivery
   field :user_id, :type => String
   belongs_to :user
   field :vendor_id, :type => String
+  field :case_no, :type => Integer
+  field :temp, :type => Float
+  field :contaminate, :type => Integer
+  field :smell, :type => Integer
+  field :physical, :type => String
+  field :approved_kg, :type => Float
+  field :rejected_kg, :type => Float
+  field :inspector, :type => String
+  field :remark, :type => String
+  field :picture, :type => String
   # mindapp end
 end
